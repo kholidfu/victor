e0a18ae035fbde5c24127e1622f616f4be72add9
sopier <sopier@gmail.com>
2014-01-14 05:16:59 +0700

    code edit

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import pycurl
 from cStringIO import StringIO
 import json
+import urllib
+from pprint import pprint
+
 
 apikey = "RutmEhKIK8vsUH8YfcVSmUSgfHzyd6Nu"
 keyword = "glass"
 
-url = "http://"+apikey+"@api.fotolia.com/Rest/1/search/getSearchResults?search_parameters%5Bwords%5D="+keyword+"&search_parameters%5Blanguage_id%5D=2&search_parameters%5Blimit%5D=2"
+url = "http://"+apikey+"@api.fotolia.com/Rest/1/search/getSearchResults?"
+
+data = {
+    "search_parameters[words]": keyword,
+    "search_parameters[language_id]": 2,
+    "search_parameters[limit]": 2,
+    }
 
 buf = StringIO()
 
 c = pycurl.Curl()
-c.setopt(c.URL, url)
+c.setopt(c.URL, url + urllib.urlencode(data))
 c.setopt(c.WRITEFUNCTION, buf.write)
 c.perform()
 
 html = buf.getvalue()
-from pprint import pprint
+
 pprint(json.loads(html))
 buf.close()
